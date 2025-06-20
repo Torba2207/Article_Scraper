21f5405d00a463133fc1ebc6e5139734c22b837e
Oleksandr Nychyporchuk <alexnychyporchuk@gmail.com>
2025-06-20 17:43:38 +0200

    init: basic main

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Web Scraper API",
+    description="API for scraping using Selenium",
+    version="0.1.0"
+)
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Scraper API!"}

3ceb78f41a04690ecaf348f2fe2ecdafc2c64b43
Oleksandr Nychyporchuk <alexnychyporchuk@gmail.com>
2025-06-21 01:20:58 +0200

    feat: add CRUD operations for Source model in crud.py

---
+++ app/crud.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import Session
+from . import models
+from . import schemas
+
+
+def get_source(db: Session, source_id: int):
+    return db.query(models.Source).filter(models.Source.id == source_id).first()
+
+def get_source_by_name(db: Session, name: str):
+    return db.query(models.Source).filter(models.Source.name == name).first()
+
+def get_sources(db:Session, skip: int=0, limit: int=100):
+    return db.query(models.Source).offset(skip).limit(limit).all()
+
+def create_source(db: Session, source: schemas.SourceCreate):
+    db_source = models.Source(name=source.name, url=str(source.url))
+    db.add(db_source)
+    db.commit()
+    db.refresh(db_source)
+    return db_source

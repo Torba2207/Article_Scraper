dd80d9295abbacc1238ebe8aed8cadf16a893fd1
Oleksandr Nychyporchuk <alexnychyporchuk@gmail.com>
2025-06-20 19:16:08 +0200

    feat: initialize database tables with SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from database import engine, Base
+from .models import source
+
+
+Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI(
     title="Web Scraper API",
